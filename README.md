bfe5931a2f887336e507d6a5b041808d11182d67
Alain Yankap <alainyankap@gmail.com>
2026-03-05 18:26:18 -0700

    feat: challenge done

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python DaisyUI Challenge
+
+## Use DaisyUI
+
+| no. | Task Description | Status |
+|-----|------------------|--------|
+| 1 | Create Markdown Sheet and make basic task list| ✅ Done |
+| 2 | Create template.html (This is the base HTML code that will be copied) | ✅ Done |
+| 3 | Create index.html (This is the file that will be overwritten) | ✅ Done |
+| 4 | Create html_editing.py | ✅ Done |
+| 5 | Update html_editing.py to use template.html and overwrite index.html| ✅ Done |
---
+++ html_editing.py
@@ -0,0 +1,80 @@
+def create_website(template_file, html_output):
+    """Function that reads the contents of a template html file
+    dynamically update its content, and outputs into an index.html file
+
+    Args:
+        template_file (string): path to the template.html file
+        html_output (string): path to the index.html file
+    """
+    
+    # Read template.html file and store boilerplate code
+    html_base = ""
+    with open(template_file, "r") as website:
+        html_base = website.read()
+
+    # Update Title
+    new_title = "My Website"
+    modified_html = html_base.replace("<title>Document", f"<title>{new_title}")
+    
+    # Implement DaisyUI
+    daisy_ui = """
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    """
+    
+    modified_html = modified_html.replace("</head>", f"{daisy_ui}</head>")
+    
+    # Helper function to add element before body tag and return modified_html
+    def add_to_body(modified_html, element_to_add):
+        
+        modified_html = modified_html.replace("</body>", f"{element_to_add}</body>")
+        return modified_html
+    
+    # Apply Theme
+    data_theme = "halloween"
+    modified_html = modified_html.replace('<html lang="en">',
+                                          f'<html lang="en" data-theme="{data_theme}">')
+    
+    # Add Title to Body
+    h1_tag = "<h1>Welcome to My Website</h1>"
+    modified_html = add_to_body(modified_html, h1_tag)
+    
+   
+
+    # Add card to html    
+    card = f"""
+         <div class="card lg:card-side bg-base-100 shadow-sm">
+            <figure>
+                <img
+                src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+                alt="Album" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">New album is released!</h2>
+                <p>Click the button to listen on Spotiwhy app.</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+                </div>
+            </div>
+        </div>     
+        """
+    modified_html = add_to_body(modified_html, card)
+
+    # Add footer to html
+    foot = f"""
+        <p>  </p>
+        <p>  </p>
+        <footer class="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4">
+            <aside class="grid-flow-col items-center">    
+                <p>Copyright ©2026 - All right reserved</p>
+            </aside>            
+        </footer>
+        """
+    modified_html = modified_html.replace("</body>", f"{foot}</body>")
+    
+    # Write index.html file after updating with Python
+    with open(html_output, "w") as file:
+        file.write(modified_html)
+    
+create_website("./template.html", "./index.html")
