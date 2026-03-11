23bd91a0435050c17425675aff2ffed669689727
Alain Yankap <alainyankap@gmail.com>
2026-03-11 13:47:11 -0600

    feat: Done new challenge generate html page with table of product

--- README.md
+++ README.md
@@ -9,3 +9,31 @@
 | 3 | Create index.html (This is the file that will be overwritten) | ✅ Done |
 | 4 | Create html_editing.py | ✅ Done |
 | 5 | Update html_editing.py to use template.html and overwrite index.html| ✅ Done |
+
+
+# Python Fetching Data and Manipulation Challenge
+
+## Concepts Used:
+1. Importing `requests` module to send requests
+2. Importing `json` module to handle JSON Decode Errors
+3. Using `for` loops to loop over fetched data and append to html
+4. Use `.replace()` to append `html` elements into `<title>` `<head>` and `<body>` elements
+5. Use `try/except` statements to control flow of errors and exceptions
+6. Use `tailwindcss` to style the tables
+
+### Steps:
+| # | Task | Status |
+|---|------|--------|
+| **1** | Use a html file with an empty body tag and the Daisy UI links. |✅|
+| **2** | Create the html table using a for loop and the data optained from the freshco data. |✅|
+| **3** | Select a Daisy UI table component of yours |✅|
+| **4** | Use name, price, and url of the picture |✅|
+| **5** | Display the url links (`href="link"`) as a pictures with a size of 50 x 50 pixels (`witdth="50" height="50"` adjust as needed) in a html img tag. |✅|
+| **6** | Add an `h1` tag on top of the table. |✅|
+| **7** | Add 2 square buttons in a new column to the right of the pictures, a red called delete and a green one called edit. |✅|
+| **8** | Write the code manually with one of 2 rows, until it looks good to your and then go over all the items and create the full string table. |✅|
+| **9** | Insert the table string in the body of the html file. |✅|
+| **10** | Read and write files as necessary.  |✅|
+| **11** | Check the `pandas...html` files in a browser as ugly reference of what should obtain, use the Daisy UI theme of your choice. |✅|
+| **12** | Deliver the python file in a new repo and provide a link to github pages, don't forget the readme. |✅|
+| **13** | Feel free to use more Daisy UI elements. |✅|
--- product_html_editing.py
+++ product_html_editing.py
@@ -1,5 +1,6 @@
-import requests
+import requests 
 import json
+import pandas as pd
 # link obtained from freshco website flyer section using developer tools
 url_freshco_mar_5_11_2026 = "https://dam.flippenterprise.net/flyerkit/publication/7813184/products?display_type=all&locale=en&access_token=881f0b9feea3693a704952a69b2a037a"
 
@@ -19,22 +20,87 @@ def get_data(url):
 
 # calling the previous function to get the full data into a variable called data_freshco
 data_freshco = get_data(url_freshco_mar_5_11_2026)
-print(len(data_freshco)) # to know how many products we have
-print(type(data_freshco)) # to check what king of data structure we have (list)
-print(data_freshco[0]) # to explore the one item in the list, user data_freshco[start_index:end_in
+#print(len(data_freshco)) # to know how many products we have
+#print(type(data_freshco)) # to check what king of data structure we have (list)
+#print(data_freshco[0]) # to explore the one item in the list, user data_freshco[start_index:end_in
+#for key,  value in data_freshco[0].items():
+#   print(key)
 
+# use of list comprehension create a list of dictionaries to select only 3 elements from every item -> name, price, pic, image_url
+results_Freshco = [{'name': item['name'], 'price': item['price_text'], 'pic': item['image_url']} for item in data_freshco]
+#print(type(results_Freshco))
+
+###creating a dataframe
+dataframe = pd.DataFrame(results_Freshco)
+print(dataframe.head())
+
+#generating a html table from a dataframe     ---- use pandas library
+html_table = dataframe.to_html()
+
+#generating a html file
+#with open("pandas_table.html", "w") as table:
+#   table.write(html_table)
+
+
+# displawying the image in the table
+# helper function to change url link
+def format_as_img(url):
+    return f'<img src="{url}" width="100">'
+
+# modifying the dataframe
+#styled_df = dataframe.style.format({'pic': format_as_img})
+
+# creating table
+#html_table_img = styled_df.to_html()
+
+# writing new files with pics
+#with open("pandas_table_img.html", "w") as table:
+#  table.write(html_table_img)
+
+
+#data = results_Freshco[0]
+
+button_tag = f"""
+    <td>
+        <button class="btn btn-success">Edit</button>
+        <button class="btn btn-error">Delete</button>
+    </td>
+    \n
+"""
+#core_table += f"<td> {index} </td> \n"
+core_table = f""
+for index, data in enumerate(results_Freshco):   
+   core_table += f"<tr> \n"   
+   core_table += f"<td> {index+1} </td> \n"
+   for key, value in data.items():
+      if key == "pic":
+        img_tag = f'<img src="{value}" width="50" height="50">'
+        core_table += f"""
+            <td>
+            <a href="link">
+                <div class="flex items-center gap-3">
+                    <div class="avatar">
+                        <div class="mask mask-squircle h-12 w-12">
+                            {img_tag}
+                        </div>
+                    </div>                
+                </div>
+            </a>
+            </td>            
+        """
+      else:        
+        core_table += f"<td>{value}</td>  \n" 
+     
+   core_table += button_tag
+   core_table += f"</tr>  \n"
+   
+
+#print(core_table)
 
 
 
 def create_website(template_file, html_output):
-    """Function that reads the contents of a template html file
-    dynamically update its content, and outputs into an index.html file
-
-    Args:
-        template_file (string): path to the template.html file
-        html_output (string): path to the index.html file
-    """
-    
+     
     # Read template.html file and store boilerplate code
     html_base = ""
     with open(template_file, "r") as website:
@@ -60,39 +126,49 @@ def create_website(template_file, html_output):
         return modified_html
     
     # Apply Theme
-    data_theme = "halloween"
-    modified_html = modified_html.replace('<html lang="en">',
-                                          f'<html lang="en" data-theme="{data_theme}">')
+    #data_theme = "halloween"
+    #modified_html = modified_html.replace('<html lang="en">',
+    #                                     f'<html lang="en" data-theme="{data_theme}">')
     
     # Add Title to Body
-    h1_tag = "<h1>Welcome to My Website</h1>"
+    h1_tag = '\n <h1 class="font-bold text-center text-lg"> Welcome to Frescho list product page </h1>'
     modified_html = add_to_body(modified_html, h1_tag)
     
    
 
-    # Add card to html    
-    card = f"""
-         <div class="card lg:card-side bg-base-100 shadow-sm">
-            <figure>
-                <img
-                src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-                alt="Album" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
-                <div class="card-actions justify-end">
-                <button class="btn btn-primary">Listen</button>
-                </div>
-            </div>
-        </div>     
+    # Add table html    
+    head_table = f"""
+         <div class="overflow-x-auto">
+            <table class="table">               
+                <thead>
+                    <tr> 
+                        <th>#</th>                     
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>image</th>     
+                        <th>Actions</th>                  
+                    </tr>
+                </thead>
+                <tbody>
         """
-    modified_html = add_to_body(modified_html, card)
+    
+    foot_table = f"""
+                </tbody>
+            </table>
+        </div>
+
+    """
+     
+    modified_html = add_to_body(modified_html, head_table)
+    modified_html = add_to_body(modified_html, core_table)
+    modified_html = add_to_body(modified_html, foot_table)
+
+
 
     # Add footer to html
     foot = f"""
-        <p>  </p>
-        <p>  </p>
+        \n
+        \n
         <footer class="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4">
             <aside class="grid-flow-col items-center">    
                 <p>Copyright ©2026 - All right reserved</p>
@@ -105,4 +181,4 @@ def create_website(template_file, html_output):
     with open(html_output, "w") as file:
         file.write(modified_html)
     
-#create_website("./template.html", "./index.html")
+create_website("./template.html", "./index.html")
